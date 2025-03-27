9beff532fc92df94b72d504be5b78810a4328885
SatrioHalim <satriohalim21@gmail.com>
2025-03-27 17:20:23 +0700

    Study Case Read Add Delete with CSV

---
+++ Expert/Study Case Farm And Poultry/index.js
@@ -0,0 +1,9 @@
+const command = process.argv[1];
+const params = process.argv[2];
+const Farm = require("./Farm.js");
+
+// Farm.readCSV();
+Farm.total();
+// Farm.addPoultry("chicken",5000);
+// Farm.sellPoultry(3);
+// console.log(Farm);
