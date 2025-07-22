ce68971989c78a5b44f83790b296d99fc18afe54
SatrioHalim <satriohalim21@gmail.com>
2025-07-23 06:00:42 +0700

    Hemat pangkal kaya part III

---
+++ Expert/Study Case Hemat Pangkal Kaya/js/script.js
@@ -0,0 +1,18 @@
+// DOM Button
+let btnIncome = document.getElementById('btnIncome');
+let btnExpense = document.getElementById('btnExpense');
+
+// tBody table
+let tBodyInc = document.getElementById('tBody-inc');
+let tBodyExp = document.getElementById('tBody-exp');
+
+function incomeHandler() {
+    console.log('Income button clicked');
+}
+function expenseHandler() {
+    console.log('Expense button clicked');
+}
+
+// Add Event Listener
+btnIncome.addEventListener('click', incomeHandler);
+btnExpense.addEventListener('click', expenseHandler);
