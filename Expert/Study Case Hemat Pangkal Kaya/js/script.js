2ccd432e5bdf3c624996d22d0e8e2d0663282fce
SatrioHalim <satriohalim21@gmail.com>
2025-07-30 12:55:51 +0700

    Study Case Hemat Pangkal Kaya part V

--- Expert/Study Case Hemat Pangkal Kaya/js/script.js
+++ Expert/Study Case Hemat Pangkal Kaya/js/script.js
@@ -2,14 +2,33 @@
 let btnIncome = document.getElementById('btnIncome');
 let btnExpense = document.getElementById('btnExpense');
 
-// tBody table
-let tBodyInc = document.getElementById('tBody-inc');
-let tBodyExp = document.getElementById('tBody-exp');
 
 // Global Variable
 let transactions = {
-    income : [],
-    expense : []
+    income : [
+        {
+            id: 1,
+            transaction: 'Gaji',
+            balance: 5000000
+        },
+        {
+            id: 2,
+            transaction: 'Bonus',
+            balance: 1000000
+        }
+    ],
+    expense : [
+        {
+            id: 1,
+            transaction: 'Makan',
+            balance: 200000
+        },
+        {
+            id: 2,
+            transaction: 'Transportasi',
+            balance: 150000 
+        }
+    ]
 }
 
 // Handle Income Transaction
@@ -31,6 +50,8 @@ function incomeHandler() {
         balance : valueBalance
     }
     transactions.income.push(tempObj);
+    getTransaction();
+    balanceCount();
     alert(`"${valueTransactionName}" dengan saldo ${valueBalance} telah ditambahkan ke daftar pemasukan`);
     console.log(transactions);
 }
@@ -53,10 +74,61 @@ function expenseHandler() {
         balance : valueBalance
     }
     transactions.expense.push(tempObj);
+    getTransaction();
+    balanceCount();
     alert(`"${valueTransactionName}" dengan saldo ${valueBalance} telah ditambahkan ke daftar pengeluaran`);
     console.log(transactions);
+}
+
+function getTransaction(){
+    // tBody table
+    let tBodyInc = document.getElementById('tBody-inc');
+    let tBodyExp = document.getElementById('tBody-exp');
+    tBodyInc.innerHTML = '';
+    tBodyExp.innerHTML = '';
+   
+    transactions.income.forEach((inc) => {
+        tBodyInc.innerHTML += `<tr>
+            <td>${inc.id}</td>
+            <td>${inc.transaction}</td>
+            <td>${inc.balance}</td>
+        </tr>`
+    });
+
+    transactions.expense.forEach((exp) => {
+        tBodyExp.innerHTML += `<tr>
+            <td>${exp.id}</td>
+            <td>${exp.transaction}</td>
+            <td>${exp.balance}</td>
+        </tr>` 
+    });
+}
+
+function balanceCount(){
+    let balanceIncome = 0;
+    let balanceExpense = 0;
+
+    transactions.income.forEach((inc) => {
+        // tanda + untuk mengkonversi string ke number
+        balanceIncome += +inc.balance;
+    });
+
+    transactions.expense.forEach((exp) => {
+        balanceExpense += +exp.balance;
+    });
+
+    let deviation = balanceIncome - balanceExpense;
+    
+    let saldoValue = document.getElementById('saldo-value');
+    saldoValue.innerHTML = deviation;
+
+    // console.log(`Total Pemasukan: ${balanceIncome}`);
+    // console.log(`Total Pengeluaran: ${balanceExpense}`);
+    // console.log(`Selisih: ${deviation}`);
 }
 
 // Add Event Listener
 btnIncome.addEventListener('click', incomeHandler);
 btnExpense.addEventListener('click', expenseHandler);
+getTransaction();
+balanceCount();
