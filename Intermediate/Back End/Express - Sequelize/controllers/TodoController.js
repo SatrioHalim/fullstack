4d193f1fa7a1a5092148ff51427c6a7bc126a267
SatrioHalim <satriohalim21@gmail.com>
2025-08-21 15:26:39 +0700

    Fixing Syntax & try using postman

--- Intermediate/Back End/Express - Sequelize/controllers/TodoController.js
+++ Intermediate/Back End/Express - Sequelize/controllers/TodoController.js
@@ -1,10 +1,24 @@
 class TodoController {
     static getTodos(req,res){
-        res.send('Main page Todos');
+        const arrObjects = [
+            {
+                id:1,
+                title: 'Belajar Node.js',
+            },{
+                id:2,
+                title: 'Belajar Express.js',
+            },{
+                id:3,
+                title: 'Belajar Sequelize',
+            }
+        ]
+        res.json(arrObjects);
     }
 
     static addTodo(req,res){
-        res.send('Add Todo Page');
+        res.json({
+            message: 'Todo item added successfully',
+        });
     }
 }
 
