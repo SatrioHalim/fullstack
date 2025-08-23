fc041e963884153685f0633e8ce6c33a608b593e
SatrioHalim <satriohalim21@gmail.com>
2025-08-23 16:09:55 +0700

    Make a model & trying migrate to db

---
+++ Intermediate/Back End/Express - Sequelize/migrations/20250823085443-create-todo.js
@@ -0,0 +1,33 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  // up => untuk membuat tabel
+  // down => untuk menghapus tabel
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      task: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('todos');
+  }
+};
