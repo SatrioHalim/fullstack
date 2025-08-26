230025dd824fd57e3035a8ed368966f8bb0c650b
SatrioHalim <satriohalim21@gmail.com>
2025-08-27 06:28:23 +0700

    Trying some validation in sequelize & add findById feature

--- Intermediate/Back End/Express - Sequelize/controllers/TodoController.js
+++ Intermediate/Back End/Express - Sequelize/controllers/TodoController.js
@@ -3,7 +3,19 @@ const {todo} = require('../models');
 class TodoController {
 
     static getTodos(req,res){
-        todo.findAll()
+        todo.findAll({
+            // Kalau mau kasih kondisi atau validasi bisa kayak gini
+            // where: {
+            //     id:1
+            // }
+
+            // Kalau mau diurutkan bisa kayak gini 
+            // ASC = ascending (dari kecil ke besar)
+            // DESC = descending (dari besar ke kecil)
+            // order: [
+            //     ['id', 'DESC']
+            // ]            
+        })
         .then(todos => {
             res.json(todos);
         })
@@ -25,6 +37,22 @@ class TodoController {
             res.json(err);
         })
         
+    }
+
+    static findById(req,res){
+        // "+" convert string to number
+        let id = +req.params.id;
+        todo.findByPk(id)
+        .then(result=>{
+            // validasi kalau data ga ketemu
+            if(!result){
+                res.json("Data not found");
+            } else {
+                res.json(result);
+            }
+        }).catch(err => {
+            res.json(err);
+        })
     }
 }
 
--- Intermediate/Back End/Express - Sequelize/routes/todo.js
+++ Intermediate/Back End/Express - Sequelize/routes/todo.js
@@ -7,4 +7,6 @@ todoRoute.get('/', TodoController.getTodos);
 // POST METHOD -> create
 todoRoute.post('/add', TodoController.addTodo);
 
+todoRoute.get('/find/:id', TodoController.findById);
+
 module.exports = todoRoute;
