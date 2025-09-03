aabbed385a91a1db669cbcc554611a15953d3399
SatrioHalim <satriohalim21@gmail.com>
2025-09-03 15:58:56 +0700

    Try build some components and init react framework

---
+++ Intermediate/Front End/learn-react/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Heading from './components/Heading'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Heading></Heading>
+      <Heading></Heading>
+      <Heading></Heading>
+      <Heading></Heading>
+    </div>
+  );
+}
+
+export default App;
---
+++ Intermediate/Front End/learn-react/src/components/Heading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Heading(){
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <p>this is paragraph</p>
+            <hr/>
+        </div>
+    ) 
+}
+
+export default Heading;
