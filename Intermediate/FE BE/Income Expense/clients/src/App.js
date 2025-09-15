00a955ca2635d54c785874c910958249991c31b9
SatrioHalim <satriohalim21@gmail.com>
2025-09-15 12:42:17 +0700

    New Study Case and some different method on export things

---
+++ Intermediate/FE BE/Income Expense/clients/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { 
+  Title, 
+  InputForm } from './components/Index';
+    
+function App() {
+  return (
+    <div className="App">
+      <Title />
+      <InputForm />
+    </div>
+  );
+}
+
+export default App;
